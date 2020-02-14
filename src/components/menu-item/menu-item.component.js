a4d58c3f401459120fe751980bc38001d3263fe7
Tayte Boss <boss.tayte@gmail.com>
2020-02-14 13:11:36 +1100

    home page into smaller components, finished styling menu component

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './menu-item.styles.scss'
+
+export default function MenuItem({ title, imageUrl, size }) {
+    return(
+        <div 
+            className={`${size} menu-item`}
+        >
+        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
+        <div className='content'>
+            <h1 className="title">{ title.toUpperCase() }</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+        </div>
+    )
+}
